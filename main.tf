9bd5945e0674af6598ff29cb202bd549243a5667
Hafsa Begum <hafsabegum0357@gmail.com>
2026-06-15 20:50:26 +0530

    Update S3 bucket name and add tags

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
 resource "aws_s3_bucket" "demo" {
-  bucket = "intern-demo-bucket-123456789"
+  bucket = "hafsa-terraform-task-demo-2026"
+
+  tags = {
+    Name        = "terraform-task-demo"
+    Environment = "dev"
+    Owner       = "Hafsa"
+  }
 }
